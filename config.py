04c7f3e36e19fb014b18352196a175b963fdd226
mrkqkk-art <mrkqkk@gmail.com>
2026-07-31 10:50:47 +0330

    Add configuration settings for bot and admin

--- config.py
+++ config.py
@@ -3,12 +3,35 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# توکن ربات تلگرام
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+
+# آیدی عددی ادمین‌ها
 ADMIN_IDS = [
     int(os.getenv("ADMIN_ID", 0))
 ]
 
+
+# کانال عضویت اجباری
 CHANNELS = [
     "@RoXeT_VpN"
 ]
+
+
+# تنظیمات پنل پاسارگاد
+PASARGAD_URL = os.getenv(
+    "PASARGAD_URL",
+    ""
+)
+
+PASARGAD_TOKEN = os.getenv(
+    "PASARGAD_TOKEN",
+    ""
+)
+
+
+# تنظیمات ربات
+BOT_NAME = "MirzaBot"
+
+VERSION = "1.0"
