6a3b37e39cd63a9c0781b57d4ea34f5a35f3fe2f
mrkqkk-art <mrkqkk@gmail.com>
2026-07-31 10:54:35 +0330

    Update services.py

--- services.py
+++ services.py
@@ -1,21 +1,44 @@
-services = []
+from database import add_service, get_user_services
 
 
-def add_service(
+def create_service(
     user_id,
-    username,
+    name,
+    config,
     volume,
     days
 ):
+    add_service(
+        user_id,
+        name,
+        config,
+        volume,
+        days
+    )
 
-    services.append({
-        "user": user_id,
-        "username": username,
-        "volume": volume,
-        "days": days
-    })
+
+def user_services(user_id):
+
+    return get_user_services(
+        user_id
+    )
 
 
-def get_services():
+def format_services(services):
 
-    return services
+    if not services:
+        return "📦 هیچ سرویسی وجود ندارد."
+
+    text = "📦 سرویس‌ها:\n\n"
+
+    for service in services:
+
+        text += (
+            f"🔹 نام: {service[2]}\n"
+            f"🌐 کانفیگ: {service[3]}\n"
+            f"📊 حجم: {service[4]}\n"
+            f"⏳ مدت: {service[5]} روز\n"
+            f"📌 وضعیت: {service[6]}\n\n"
+        )
+
+    return text
