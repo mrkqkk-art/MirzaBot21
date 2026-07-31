5bfee7b5a765f726b4a8098b4865bd0ce2fe7261
mrkqkk-art <mrkqkk@gmail.com>
2026-07-31 10:55:17 +0330

    Update ‌admin_panel.py

--- ‌admin_panel.py
+++ ‌admin_panel.py
@@ -1,28 +1,26 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from buttons import admin_menu
 from database import users_count
 
 
-async def admin_menu(update, context):
+async def show_admin_panel(update, context):
 
     count = users_count()
 
-    buttons = [
-        [
-            InlineKeyboardButton(
-                "📊 آمار کاربران",
-                callback_data="stats"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                "📢 ارسال همگانی",
-                callback_data="broadcast"
-            )
-        ]
-    ]
-
     await update.message.reply_text(
         f"👑 پنل مدیریت\n\n"
-        f"👥 تعداد کاربران: {count}",
-        reply_markup=InlineKeyboardMarkup(buttons)
+        f"👥 تعداد کاربران: {count}\n\n"
+        f"یکی از گزینه‌ها را انتخاب کنید:",
+        reply_markup=admin_menu()
+    )
+
+
+async def admin_stats(update, context):
+
+    query = update.callback_query
+
+    count = users_count()
+
+    await query.message.reply_text(
+        f"📊 آمار ربات\n\n"
+        f"👥 کاربران: {count}"
     )
