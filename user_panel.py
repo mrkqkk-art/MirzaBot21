14e3944192620c6dc7b61b606af6fa3d8a76c4c1
mrkqkk-art <mrkqkk@gmail.com>
2026-07-31 10:53:28 +0330

    Update user_panel.py

--- user_panel.py
+++ user_panel.py
@@ -1,10 +1,36 @@
-from buttons import main_menu
+from buttons import user_menu
+from database import get_user_services
 
 
-async def user_panel(update, context):
+async def show_user_panel(update, context):
 
     await update.message.reply_text(
         "👤 پنل کاربری\n\n"
-        "از منوی زیر استفاده کنید:",
-        reply_markup=main_menu()
+        "یکی از گزینه‌ها را انتخاب کنید:",
+        reply_markup=user_menu()
     )
+
+
+async def my_services(update, context):
+
+    query = update.callback_query
+    user_id = query.from_user.id
+
+    services = get_user_services(user_id)
+
+    if not services:
+        await query.message.reply_text(
+            "📦 شما هنوز سرویسی ندارید."
+        )
+        return
+
+    text = "📦 سرویس‌های شما:\n\n"
+
+    for s in services:
+        text += (
+            f"🔹 نام: {s[2]}\n"
+            f"📊 حجم: {s[4]}\n"
+            f"⏳ مدت: {s[5]} روز\n\n"
+        )
+
+    await query.message.reply_text(text)
